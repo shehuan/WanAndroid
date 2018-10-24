2a7addd6e4c3f4944a26c78b7bf4eb46a1065b28
shehuan <shehuandev@163.com>
2018-10-24 16:19:36 +0800

    优化泛型

--- app/src/main/java/com/shehuan/keasymvp/base/net/convert/ExceptionConvert.kt
+++ app/src/main/java/com/shehuan/keasymvp/base/net/convert/ExceptionConvert.kt
@@ -5,8 +5,8 @@ import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.functions.Function
 
-class ExceptionConvert<E> : Function<Throwable, ObservableSource<E>> {
-    override fun apply(throwable: Throwable): ObservableSource<E> {
+class ExceptionConvert<E> : Function<Throwable, ObservableSource<out E>> {
+    override fun apply(throwable: Throwable): ObservableSource<out E> {
         return Observable.error(ExceptionHandler.handle(throwable))
     }
 }
